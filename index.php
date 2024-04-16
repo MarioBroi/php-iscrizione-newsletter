7aa7a0d71a03ab5ece9487838ea8b2faa91137eb
MarioBroi <inoix16@gmail.com>
2024-04-16 12:54:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo('test');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iscrizione newsletter</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
